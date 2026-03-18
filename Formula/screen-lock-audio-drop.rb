f430737ffb749767005cd2cc2c761d3ddc76eb02
Alex Carol <acarolcampre@adobe.com>
2026-03-18 09:20:39 +0100

    Update formula to use v0.3.0 tag

--- Formula/screen-lock-audio-drop.rb
+++ Formula/screen-lock-audio-drop.rb
@@ -1,8 +1,8 @@
 class ScreenLockAudioDrop < Formula
   desc "Disconnect Bluetooth audio devices when your Mac screen locks"
   homepage "https://github.com/alexcarol/mac-screen-lock-audio-drop"
-  url "https://github.com/alexcarol/mac-screen-lock-audio-drop/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/alexcarol/mac-screen-lock-audio-drop/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "3281f4b930e30db2838eb014f8f538656b9660021c0dbe879be5e25530c3df2c"
   license "MIT"
 
   depends_on :macos
@@ -19,8 +19,16 @@ class ScreenLockAudioDrop < Formula
     error_log_path var/"log/screen-lock-audio-drop.err"
   end
 
+  def post_install
+    system opt_bin/"screen-lock-audio-drop", "--check-permissions"
+  end
+
   def caveats
     <<~EOS
+      Bluetooth permission is required to disconnect audio devices on screen lock.
+      If you were not prompted during install, grant access manually:
+        System Settings > Privacy & Security > Bluetooth
+
       To start screen-lock-audio-drop now and restart at login:
         brew services start screen-lock-audio-drop
 
